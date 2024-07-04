c216b1301c4439816e5aaa2fe663e3550198e82a
ErdyFai(071) <145762242+Erdyfai@users.noreply.github.com>
2024-07-04 17:53:43 +0700

    Exercise 1

---
+++ Inisiasi Object/src/Singa.java
@@ -0,0 +1,11 @@
+
+public class Singa {
+	String name;
+	String color = "White";
+	String gender = "Man";
+	int age;
+	public void roar(String name) {
+		this.name = name;
+		System.out.println("Hey "+ name +" *RAULLLLL*");
+	}
+}
